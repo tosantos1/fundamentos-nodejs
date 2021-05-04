bbf30a012786871dfdb83fa8ec775dd4dfb37ed2
Tiago Oliveira <tiagolivesan@outlook.com>
2021-05-04 19:52:03 -0300

    node js

--- src/index.js
+++ src/index.js
@@ -2,26 +2,8 @@ const express = require("express");
 
 const app = express();
 
-//localhost:3333
-
-app.get("/courses", (request, response) => {
-	return response.json(["Curso 1", "Curso 2", "Curso 3",]);
-});
-
-app.post("/courses", (request, response) => {
-	return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.put("/courses/:id", (request, response) => {
-	return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (request, response) => {
-	return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (request, response) => {
-	return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+app.post("/account", (request, response) => {
+	const { cpf, name } = request.body;
 });
 
 app.listen(3333);
